522ac972599b403495a6db2ec99307d3c83ac02c
DevJesus-debug <jesusterrazasdev25@gmail.com>
2022-03-23 20:40:39 -0700

    creating the functionalities

--- app.js
+++ app.js
@@ -1,25 +1,61 @@
-const container = document.querySelector(".container");
+const container = document.querySelector(".card-container");
 const input = document.querySelector(".input");
 const search = document.querySelector(".search");
+const allPokemonCont = document.querySelector(".all-pokemon-cont");
+const allPokemonBtn = document.querySelector(".all-pokemon-btn");
 
 
+function createHtml(data){
+    const html = document.createElement("div");
+    html.classList = "pokemon-img"
+    html.innerHTML = `<img src="${data.sprites.front_default}">`;
+    container.appendChild(html);
+}
 
-
- async function getPokemon(result){
-     const dataFetch = await fetch(`https://pokeapi.co/api/v2/pokemon/${result}/`)
-     const data = await dataFetch.json();
-    
-     const html = document.createElement("div");
-     html.classList = "pokemon-img"
-     html.innerHTML = `<img src="${data.sprites.front_default}">`;
-     container.appendChild(html);
+ async function getPokemon(){
+     result = input.value
+     const dataFetch = await fetch(`https://pokeapi.co/api/v2/pokemon/${result}`)
+     const data = await dataFetch.json();    
+     createHtml(data);
+     
  }
 
  search.addEventListener("click", (e)=>{
+     e.preventDefault();
+     clear();
+     getPokemon();
+ });
+
+ function clear(){
+     container.innerHTML = "";
+     allPokemonCont.innerHTML= "";
+ }
+
+ async function getPokemons(){
+    for(let i = 1; i< 100 ; i++){
+    const dataFetch = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+    const data = await dataFetch.json();  
+
+    const pokemonInfo = `<img src="${data.sprites.front_default}">
+                            <div class="name">
+                                <div class="number">${data.id}.</div>
+                                <h2>${data.name}</h2>
+                            </div>
+                        <div class="types">${data.types.map((type) => type.type.name)}</div>
+                        `               
+    const pokeCard = document.createElement("div");
+    pokeCard.classList.add("poke-card")
+    pokeCard.innerHTML = pokemonInfo;
+    allPokemonCont.appendChild(pokeCard)
+    }
+}
+
+allPokemonBtn.addEventListener("click", (e)=>{
     e.preventDefault();
-    result = input.value
-    getPokemon(result)
- })
+    clear()
+    getPokemons();
+});
+
 
 
 
