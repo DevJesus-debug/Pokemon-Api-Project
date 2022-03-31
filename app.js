e8b6969612cbaf0759f0a80eca39ae86c9b47230
DevJesus-debug <jesusterrazasdev25@gmail.com>
2022-03-31 15:21:23 -0700

    working on the styles

--- app.js
+++ app.js
@@ -5,17 +5,25 @@ const allPokemonCont = document.querySelector(".all-pokemon-cont");
 const allPokemonBtn = document.querySelector(".all-pokemon-btn");
 const showMoreBtn = document.querySelector(".more-pokemon");
 
-let pokemonList = 889;
+let pokemonList = 899;
+ 
 
 function createHtml(data){
-    const html = document.createElement("div");
-    html.classList = "pokemon-img"
-    html.innerHTML = `<img src="${data.sprites.front_default}">`;
-    container.appendChild(html);
+    const pokemonInfo = `<img src="${data.sprites.front_default}">
+    <div class="name">
+    <div class="number">${data.id}.</div>
+         <h2>${data.name}</h2>
+    </div>
+     <div class="types">${data.types.map((type) => type.type.name)}</div>
+    ` 
+    const pokeCard = document.createElement("div");
+        pokeCard.classList.add("poke-card")
+        pokeCard.innerHTML = pokemonInfo;
+        allPokemonCont.appendChild(pokeCard);
 }
 
- async function getPokemon(){
-     result = input.value
+ async function getPokemon(){ 
+     result = input.value;
      const dataFetch = await fetch(`https://pokeapi.co/api/v2/pokemon/${result}`)
      const data = await dataFetch.json();    
      createHtml(data);
@@ -25,6 +33,7 @@ function createHtml(data){
  function clear(){
      container.innerHTML = "";
      allPokemonCont.innerHTML= "";
+     input.value = "";
  }
 
  async function getPokemons(){
@@ -33,46 +42,16 @@ function createHtml(data){
         promises.push(fetch(`https://pokeapi.co/api/v2/pokemon/${i}`).then((res) => res.json()))
     }
 
-    await Promise.all(promises).then(results =>{
-         results.map(result =>{
-            const pokemonInfo = `<img src="${result.sprites.front_default}">
-            <div class="name">
-            <div class="number">${result.id}.</div>
-                 <h2>${result.name}</h2>
-            </div>
-             <div class="types">${result.types.map((type) => type.type.name)}</div>
-            ` 
-
-        const pokeCard = document.createElement("div");
-        pokeCard.classList.add("poke-card")
-        pokeCard.innerHTML = pokemonInfo;
-        allPokemonCont.appendChild(pokeCard)
-        })
+    await Promise.all(promises).then(datas =>{
+         datas.map(data =>{
+            createHtml(data);
+        });
     })
 }
 
 
 
 
-//  async function getPokemons(){
-//     for(let i = 1; i< 100 ; i++){
-//     const dataFetch = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
-//     const data = await dataFetch.json();  
-    
-//         const pokemonInfo = `<img src="${data.sprites.front_default}">
-//         <div class="name">
-//             <div class="number">${data.id}.</div>
-//             <h2>${data.name}</h2>
-//         </div>
-//     <div class="types">${data.types.map((type) => type.type.name)}</div>
-//     `               
-//     const pokeCard = document.createElement("div");
-//     pokeCard.classList.add("poke-card")
-//     pokeCard.innerHTML = pokemonInfo;
-//     allPokemonCont.appendChild(pokeCard)
-
-//     }
-// }
 
 showMoreBtn.addEventListener("click", (e)=>{
   
@@ -81,8 +60,8 @@ showMoreBtn.addEventListener("click", (e)=>{
 
 search.addEventListener("click", (e)=>{
     e.preventDefault();
-    clear();
-    getPokemon();
+    getPokemon(); 
+    clear()
 });
 
 allPokemonBtn.addEventListener("click", (e)=>{
--- app2.js
+++ app2.js
@@ -1,2 +1,100 @@
 
  
+const container = document.querySelector(".card-container");
+const input = document.querySelector(".input");
+const search = document.querySelector(".search");
+const allPokemonCont = document.querySelector(".all-pokemon-cont");
+const allPokemonBtn = document.querySelector(".all-pokemon-btn");
+const showMoreBtn = document.querySelector(".more-pokemon");
+
+let pokemonList = 889;
+
+function createHtml(data){
+    const pokemonInfoCard = `<img src="${data.sprites.front_default}">
+            <div class="name">
+            <div class="number">${data.id}.</div>
+                 <h2>${data.name}</h2>
+            </div>
+             <div `
+    const card = document.createElement("div");
+    card.innerHTML = pokemonInfoCard;
+    container.appendChild(card);
+}
+
+ async function getPokemon(){
+     result = input.value
+     const dataFetch = await fetch(`https://pokeapi.co/api/v2/pokemon/${result}`)
+     const data = await dataFetch.json();    
+     createHtml(data);
+     
+ }
+
+ function clear(){
+     container.innerHTML = "";
+     allPokemonCont.innerHTML= "";
+ }
+
+ async function getPokemons(){
+    const promises = [];
+    for(let i = 1; i < pokemonList ; i++){
+        promises.push(fetch(`https://pokeapi.co/api/v2/pokemon/${i}`).then((res) => res.json()))
+    }
+
+    await Promise.all(promises).then(results =>{
+         results.map(result =>{
+            const pokemonInfo = `<img src="${result.sprites.front_default}">
+            <div class="name">
+            <div class="number">${result.id}.</div>
+                 <h2>${result.name}</h2>
+            </div>
+             <div class="types">${result.types.map((type) => type.type.name)}</div>
+            ` 
+
+        const pokeCard = document.createElement("div");
+        pokeCard.classList.add("poke-card")
+        pokeCard.innerHTML = pokemonInfo;
+        allPokemonCont.appendChild(pokeCard)
+        })
+    })
+}
+
+
+
+
+//  async function getPokemons(){
+//     for(let i = 1; i< 100 ; i++){
+//     const dataFetch = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+//     const data = await dataFetch.json();  
+    
+//         const pokemonInfo = `<img src="${data.sprites.front_default}">
+//         <div class="name">
+//             <div class="number">${data.id}.</div>
+//             <h2>${data.name}</h2>
+//         </div>
+//     <div class="types">${data.types.map((type) => type.type.name)}</div>
+//     `               
+//     const pokeCard = document.createElement("div");
+//     pokeCard.classList.add("poke-card")
+//     pokeCard.innerHTML = pokemonInfo;
+//     allPokemonCont.appendChild(pokeCard)
+
+//     }
+// }
+
+showMoreBtn.addEventListener("click", (e)=>{
+  
+});
+
+
+search.addEventListener("click", (e)=>{
+    e.preventDefault();
+    clear();
+    getPokemon();
+});
+
+allPokemonBtn.addEventListener("click", (e)=>{
+    e.preventDefault();
+    clear()
+    getPokemons();
+});
+
