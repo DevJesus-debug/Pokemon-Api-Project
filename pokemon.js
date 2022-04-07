a1440ae2ada2ab484edefec0c53e9496f7a4a933
DevJesus-debug <jesusterrazasdev25@gmail.com>
2022-04-06 19:17:32 -0700

    working on some styles

--- pokemon.js
+++ pokemon.js
@@ -8,16 +8,143 @@ const showMoreBtn = document.querySelector(".more-pokemon");
 const loading = document.querySelector(".loading-icon");
 const myTeamCont = document.querySelector(".my-team-cont");
 const addTeamBtn = document.querySelector(".add-team-btn");
-const myTeamPage = document.querySelector(".my-team-page");
-
-
-
+const myTeamPageBtn = document.querySelector(".my-team-page");
+const myTeamWrapper = document.querySelector(".team-cont-wraper");
+const homeBtn = document.querySelector(".home-page");
 let pokemonList = 899;
+
+//Event Listeners
+
+myTeamPageBtn.addEventListener("click",()=>{
+    myTeamWrapper.classList.add("show")
+    mainContainer.style.display ="none";
+    clear()
+})
+
+homeBtn.addEventListener("click",()=>{
+    myTeamWrapper.classList.remove("show")
+    mainContainer.style.display ="flex";
+    clear();
+})
+
+
+search.addEventListener("click", (e)=>{
+    e.preventDefault();
+    searchPokemons()
+    clear()
+});
+
+
+
+
+allPokemonBtn.addEventListener("click", (e)=>{
+    e.preventDefault();
+    loading.classList.add("load");
+    getAllPokemons();
+    clear();
+});
+
+function clear(){ 
+    allPokemonCont.innerHTML= "";
+    input.value = "";
+}
+
+
+
+//FUNTIONS/////////////////////////////////////////
+
+
+
+async function searchPokemons(){ 
+    result = input.value;
+    const dataFetch = await fetch(`https://pokeapi.co/api/v2/pokemon/${result}`)
+    const data = await dataFetch.json();    
+    createHtml(data,allPokemonCont);
+}
+
+async function getAllPokemons(){
+    const promises = [];
+    for(let i = 1; i < pokemonList ; i++){
+        promises.push(fetch(`https://pokeapi.co/api/v2/pokemon/${i}`).then((res) => res.json()))
+    }
+
+    await Promise.all(promises).then(datas =>{
+         datas.map(data =>{
+            createHtml(data, allPokemonCont);
+        });
+    })
+    loading.classList.remove("load");
+}
+
+function createHtml(data,container,pokeCard){
+    const pokemonInfo = `
+    <div class="img-cont">
+        <img src="${data.sprites.front_default}">
+    </div>
+    <div class="info-cont">
+        <div class="name">
+            <div class="number">${data.id}.</div>
+                <h2>${upperCase(data)}</h2>
+        </div>
+        <div class="types">Type: ${data.types.map((type) => upperCase(type.type))}</div>
+        <div class="add-team-btn" onclick="pushingToTeam(${data.id})">Add to team</div>
+    </div>
+    ` 
+       pokeCard = document.createElement("div");
+        pokeCard.classList.add("poke-card")
+        pokeCard.innerHTML = pokemonInfo;
+        container.appendChild(pokeCard);
+        
+
+        typeColors(data,pokeCard)
+
+}
+
+
+async function pushingToTeam(id){ 
+    const dataFetch = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    const data = await dataFetch.json();
+    
+    if(myTeamCont.children.length < 6){
+        createHtml(data,myTeamCont)
+        const children = myTeamCont.children
+        const removeBtn = document.createElement("div");
+        removeBtn.classList.add("remove-btn")
+        const html =`<button onclick="removingTeam(${data.id},event,this)">x</button>`
+        removeBtn.innerHTML = html
+
+        Array.from(children).forEach(child=>{
+            child.appendChild(removeBtn)
+        })
+
+    }else{
+        false
+    }
+
+}
+
+
+async function removingTeam(id,e,el){
+    const dataFetch = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    const data = await dataFetch.json();
+    let parentElement =el.parentElement.parentElement;
+    e = e || window.event
+    if( e.target === el){
+        myTeamCont.removeChild(parentElement)
+        //we be the dom element 
+    }
+        // e = e || window.event
+        // console.log(e); //will be the event
+        // console.log(el.parentElement.parentElement); 
+  }
+
+
 
 function upperCase(data){
     const letter = data.name[0].toUpperCase() + data.name.slice(1)
     return letter
  }
+
 
  function typeColors(data,cards){
     //Colors of the cards 
@@ -95,113 +222,19 @@ function upperCase(data){
 }
 
 
-async function pushingToTeam(id){ 
-    const dataFetch = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-    const data = await dataFetch.json();
-    
-    if(myTeamCont.children.length < 6){
-        createHtml(data,myTeamCont)
-        const children = myTeamCont.children
-        const removeBtn = document.createElement("div");
-        const html =`<button class="remove-btn" onclick="removingTeam(${data.id},event,this)">X</button>`
-        removeBtn.innerHTML = html
 
-        Array.from(children).forEach(child=>{
-            child.appendChild(removeBtn)
-        })
-
-    }else{
-        false
-    }
-
-}
-
-
-async function removingTeam(id,e,el){
-    const dataFetch = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-    const data = await dataFetch.json();
-    let parentElement =el.parentElement.parentElement;
-    e = e || window.event
-    if( e.target === el){
-        myTeamCont.removeChild(parentElement)
-        //we be the dom element 
-    }
-        // e = e || window.event
-        // console.log(e); //will be the event
-        // console.log(el.parentElement.parentElement); 
-
-  }
 
 
  
-
- function createHtml(data,container,pokeCard){
-    const pokemonInfo = `
-    <div class="img-cont">
-        <img src="${data.sprites.front_default}">
-    </div>
-    <div class="info-cont">
-        <div class="name">
-            <div class="number">${data.id}.</div>
-                <h2>${upperCase(data)}</h2>
-        </div>
-        <div class="types">Type: ${data.types.map((type) => upperCase(type.type))}</div>
-        <div class="add-team-btn" onclick="pushingToTeam(${data.id})">Add to team</div>
-    </div>
-    ` 
-       pokeCard = document.createElement("div");
-        pokeCard.classList.add("poke-card")
-        pokeCard.innerHTML = pokemonInfo;
-        container.appendChild(pokeCard);
-        
-
-        typeColors(data,pokeCard)
-
-}
-
- async function getPokemon(){ 
-     result = input.value;
-     const dataFetch = await fetch(`https://pokeapi.co/api/v2/pokemon/${result}`)
-     const data = await dataFetch.json();    
-     createHtml(data,allPokemonCont);
- }
-
- function clear(){
-     
-     allPokemonCont.innerHTML= "";
-     input.value = "";
- }
-
- async function getAllPokemons(){
-    const promises = [];
-    for(let i = 1; i < pokemonList ; i++){
-        promises.push(fetch(`https://pokeapi.co/api/v2/pokemon/${i}`).then((res) => res.json()))
-    }
-
-    await Promise.all(promises).then(datas =>{
-         datas.map(data =>{
-            createHtml(data, allPokemonCont);
-        });
-    })
-    loading.classList.remove("load");
-}
+ 
 
 
 
-//Event Listeners
 
-search.addEventListener("click", (e)=>{
-    e.preventDefault();
-    getPokemon(); 
-    clear()
-});
 
-allPokemonBtn.addEventListener("click", (e)=>{
-    e.preventDefault();
-    loading.classList.add("load");
-    getAllPokemons();
-    clear();
-});
+
+
+
 
 
 
